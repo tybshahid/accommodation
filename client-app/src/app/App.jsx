df2642ebfbf46dca514c54142d13daf194604500
Tayyab Shahid <tshahid@mazda.com.au>
2022-11-10 13:21:54 +1100

    Add App Logo. Add App Wrapper. Add palceholder for App Features.

--- client-app/src/app/App.jsx
+++ client-app/src/app/App.jsx
@@ -1,9 +1,15 @@
 import './App.css'
+import logo from '../logo.png'
 
 const App = () => {
     return (
         <div className="App">
-          Welcome to Qantas Accommodation
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+            </header>
+            <div className="App-features">
+                Features go here
+            </div>
         </div>
     )
 }
